254c0b1b1e38e12c4c2c899269acaf2654731163
Preetishx77 <pbforlyf@gmail.com>
2022-05-26 12:13:47 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,163 @@
+import { Dropdown } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Chart from "react-apexcharts";
+
+
+let dataobj = [];
+let dataobj2 = [];
+
+function App() {
+
+
+
+  const [asset, setAsset] = useState('eth');
+  const [interval1, setInterval1] = useState('15m');
+
+
+  const [series1, setSeries1] = useState([{
+    data: []
+  }]);
+
+  const names = {
+    'eth': 'Ethereum',
+    'btc': 'Bitcoin',
+    'sol': 'Solana',
+    'dot': 'Polkadot',
+    'bnb': 'Binance',
+    'ada': 'Cardano',
+
+  }
+
+
+  useEffect(() => {
+
+
+    const conn = new WebSocket(`wss://stream.binance.com:9443/ws/${asset}eur@kline_${interval1}`);
+
+
+
+    conn.onmessage = function (event) {
+      const json = JSON.parse(event.data);
+      try {
+        if ((json.event = 'data')) {
+
+          let obj = {
+            x: json.E,
+            y: [json.k.o, json.k.h, json.k.l, json.k.c]
+          }
+          let obj2 = {
+            x: json.E,
+            open: json.k.o,
+            close: json.k.c,
+            high: json.k.h,
+            low: json.k.l,
+
+          }
+
+          dataobj.push(obj)
+          dataobj2.push(obj2)
+
+          series1[0].data = dataobj;
+          setSeries1(series1);
+          console.log(json);
+          console.log(series1);
+
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    return () => conn.close();
+  }, [asset, interval1, series1]);
+
+
+
+
+
+
+  const options = {
+    chart: {
+      type: 'candlestick',
+      height: 350
+    },
+    title: {
+      text: 'CandleStick Chart',
+      align: 'left'
+    },
+    xaxis: {
+      type: 'datetime'
+    },
+    yaxis: {
+      tooltip: {
+        enabled: true
+      }
+    }
+  }
+
+
+  return (
+    <>
+
+
+      <div style={{ display: 'flex', margin: '1rem' }}>
+        <div className="mx-2">
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Choose Asset
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              {
+                ['eth', 'btc', 'sol', 'dot', 'bnb', 'ada'].map(asset => {
+                  return <Dropdown.Item onClick={() => setAsset(asset)}>{names[asset]}</Dropdown.Item>
+                }
+                )
+
+              }
+
+
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        <div>
+
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Choose Interval
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              {
+                ['1m', '3m', '5m', '15m'].map(interval => {
+                  return <Dropdown.Item onClick={() => setInterval1(interval)}>{interval}</Dropdown.Item>
+                }
+                )
+              }
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        <div className="mx-2 py-2">
+          <h5>Asset: {names[asset]}</h5>
+        </div>
+        <div className="mx-2 py-2">
+          <h5>Interval: {interval1}</h5>
+        </div>
+        <div className="mx-2 py-2">
+          <h5>Currency: EUR </h5>
+        </div>
+      </div>
+      <div className="container">
+        <Chart options={options} series={series1} markers={{ size: 0 }} dataLabels={{ enabled: false }} type="candlestick" height={550} />
+
+      </div>
+      {
+
+      }
+
+
+    </>);
+}
+
+export default App;
